2b302c13e1987a4b27c3b830d3d4c52d41b0621b
rogueuncle <1098369048@qq.com>
2020-08-26 16:50:09 +0800

    20200826

--- View/Form1.cs
+++ View/Form1.cs
@@ -10,7 +10,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.EntityFrameworkCore;
-
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace View
 {
@@ -89,7 +90,49 @@ namespace View
                 obj.EndUpdate();
             }
         }
+
+
+        private const string ConnectionString = "Server=127.0.0.1;Database=ZhiHu2;User Id=sa;Password=sa123456789;";
+
+        #region 创建模型
+        [Table("Answer")]
+        public class Answer
+        {
+            public int Id { get; set; }
+
+            [Required]
+            [StringLength(32)]
+            public string Author_Id { get; set; }
+
+            [Required]
+            public int Question_Id { get; set; }
+
+
+        }
+
+        #endregion
+
+        #region 创建上下文
+        public class AnswerContext : DbContext
+        {
+            private const string ConnectionString = Form1.ConnectionString;
+            public DbSet<Answer> answers { get; set; }
+            //protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+            //{
+            //    //base.OnConfiguring(dbContextOptionsBuilder);
+            //    dbContextOptionsBuilder.UseSqlServer(ConnectionString);
+            //}
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=127.0.0.1;Database=ZhiHu2;User Id=sa;Password=sa123456789;");
+            }
+        }
+
+
+        #endregion
+
     }
 
-    
+
 }
---
+++ Views/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Views
+{
+    public partial class Form1 : Form
+    {
+        private const string ConnectionString = "Server=127.0.0.1;Database=WroxBooks;User Id=sa;Password=sa123456789;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var conn = new AnswerContext())
+            {
+                //conn.Database.EnsureCreated();
+                //conn.Database.Migrate();
+                var x = conn.answers.ToList();
+            }
+        }
+
+        [Table("Ans")]
+        public class Answer
+        {
+            public int Id { get; set; }
+            public string name { get; set; }
+
+            [Required]
+            [StringLength(32)]
+            public string Author_Id { get; set; }
+
+            [Required]
+            public int Question_Id { get; set; }
+        }
+
+        public class AnswerContext : DbContext
+        {
+            private const string ConnectionString = Form1.ConnectionString;
+            public DbSet<Answer> answers { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+            {
+                base.OnConfiguring(dbContextOptionsBuilder);
+                dbContextOptionsBuilder.UseSqlServer(ConnectionString);
+            }
+        }
+
+
+    }
+}
